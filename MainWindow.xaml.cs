e4351cf3be7175a82f1308f527afe0b55c4798ae
OndraMarek <czondrejmarek@gmail.com>
2025-11-09 18:30:23 +0100

    Přidejte soubory projektu.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ConvolutionalMasks
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadOriginalImage();
+        }
+        private void LoadOriginalImage()
+        {
+            Uri uri = new("/images/homer.jpg", UriKind.Relative);
+            BitmapImage bitmap = new(uri);
+            ImgOriginal.Source = bitmap;
+        }
+
+        private void BtnApplyConvolution_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
